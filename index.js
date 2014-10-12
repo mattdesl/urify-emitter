71fc0ee98083ae87a82aee5e58e427ccfd47358f
mattdesl <dave.des@gmail.com>
2014-10-11 21:11:00 -0400

    version bump

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-//HACK: 
 //browserify runs module-deps before the plugins are setup,
 //which means all the modules will already have their urlify transforms
 //already run. Instead, we need to disable it for the command-line
 //and run it through the plugin architecture as a global transform
 
-//Hopefully a better solve will be found shortly.
+//Hopefully a solve will be found shortly....
 
 var through = require('through2')
-var urify = require('urify')
+// var urify = require('urify')
 
-function noop(file, opt) {
-    return through()
-}
-urify.__emitter = noop
+// function noop(file, opt) {
+//     return through()
+// }
+
+// urify.__emitter = noop
 module.exports = require('./plugin')
 
 // module.exports.enabled = function(bool) {
